2fd8d4163b728f619127a1092097c51a24f331bf
esteban <ekramer@itba.edu.ar>
2018-06-25 10:51:37 -0300

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,25 @@ app.get('/todos', (req, res) => {
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
+	});
+});
+
+// con :id creo la variable id y la puedo acceder
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		// lo devuelvo como objeto con todo como property para tener mas flexibilidad
+		// por ejemplo agregarle mas properties
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
 	});
 });
 
